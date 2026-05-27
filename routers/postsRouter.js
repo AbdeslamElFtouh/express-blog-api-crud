635a5e20cc55b31e9a3835d0c1db46aa07067f85
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-27 16:30:04 +0200

    ADD scrittura rotte

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,3 +1,12 @@
 import express from 'express'
 import { index, show, store, modify, destroy } from '../controllers/postsController.js';
 
+const router = express.Router();
+
+router.get('/', index);
+router.get('/:id', show);
+router.post('/:id', store);
+router.patch('/', modify);
+router.delete('/:id', destroy);
+
+export default router;

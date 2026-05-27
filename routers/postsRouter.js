ab676377c37aa5f2e05b21acb7ea1cb75fef1f1c
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-27 16:24:48 +0200

    ADD Compilazione file principale (server.js) install express collegare express al file collegamento router al file messa in ascolto su porta 3000

---
+++ routers/postsRouter.js
@@ -0,0 +1,3 @@
+import express from 'express'
+import { index, show, store, modify, destroy } from '../controllers/postsController.js';
+
---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import postsRouter from './routers/postsRouter.js';
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+const url = process.env.SERVER_URL || localhost;
+
+app.use('/posts', postsRouter)
+app.use(express.json())
+
+app.listen(`${port}`, (error) =>{
+    if(error) {
+        console.error('Errore')
+        return;
+    }
+    console.log('Server avviato')
+})
+
